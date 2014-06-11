b4490c53843dc51b2d47f07ba3affc8d9e36a754
koala4js <ljc57521@gmail.com>
2014-06-11 19:59:08 +0800

    scrollTop组件

--- index.js
+++ index.js
@@ -1,43 +1,58 @@
 (function(window){
-	var toTop = document.getElementById('toTop');
-	var iTimer = null;
-	var v = 0;
+	function ScrollTop(id){
+		this.obj = document.getElementById(id);
+		this.timer = null;
+		this.init();
+	}
 
-	setTop();
-
-	window.onscroll = function(){
-
-		if(v == 2){
-			clearInterval(iTimer);
-		}
-		v = 2;
-		setTop();
-	};
-
-	toTop.onclick = function(){
-		clearInterval(iTimer);
-		var iCur = 0;
-		var iSpeed = 0;
-
-		iTimer = setInterval(function(){
-			iCur = document.documentElement.scrollTop || document.body.scrollTop;
-			iSpeed = Math.floor((0 - iCur)/8);
-
-			if(iCur == 0){
-				clearInterval(iTimer);
-			}else{
-				document.documentElement.scrollTop = document.body.scrollTop = iCur + iSpeed;
+	ScrollTop.prototype.init = function(){
+		var _this = this;
+		this.setTop();
+		this.bind(window, 'scroll', function(){
+			if(_this.v == 2){
+				clearInterval(_this.timer);
 			}
-			
-			v = 1;
+			_this.v = 2;
+			_this.setTop();
+		});
+		this.bind(this.obj, 'click', function(){
+			clearInterval(_this.timer);
+			var iCur = 0;
+			var iSpeed = 0;
 
-		},30);
+			_this.timer = setInterval(function(){
+				iCur = document.documentElement.scrollTop || document.body.scrollTop;
+				iSpeed = Math.floor((0 - iCur)/8);
 
-	};
+				if(iCur == 0){
+					clearInterval(_this.timer);
+				}else{
+					document.documentElement.scrollTop = document.body.scrollTop = iCur + iSpeed;
+				}
+				_this.v = 1;
 
-	function setTop(){
+			},30);
+		});
+	}
+
+	ScrollTop.prototype.v = 0;
+
+	ScrollTop.prototype.setTop = function(){
 		var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+		this.obj.style.top = scrollTop + document.documentElement.clientHeight - this.obj.offsetHeight + 'px';
+	}
+	
+	ScrollTop.prototype.bind = function(obj, evname, fn){
+		if(obj.addEventListener){
+			obj.addEventListener(evname, fn, false);
+		}else{
+			obj.attachEvent('on'+evname, function(){
+				fn.call(obj);
+			});
+		}
+	}
 
-		toTop.style.top = scrollTop + document.documentElement.clientHeight - toTop.offsetHeight + 'px';
+	window.scrollTop = function(id){
+		new ScrollTop(id);
 	}
 })(window);
