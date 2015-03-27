9b1996d567484238707c519802908b49dbdde2ae
koala4js <ljc57521@gmail.com>
2015-03-27 20:08:28 +0800

    1

--- pagination/js/jquery.pagination.js
+++ pagination/js/jquery.pagination.js
@@ -1,46 +1,104 @@
 (function($){
-	$.fn.pagination = function(opts){
-		opts = $.extend({}, $.fn.pagination.defaults, opts);
+	$.fn.pagination = function(opt){
+		opt = $.extend({}, $.fn.pagination.defaults, opt);
 		var _this = $(this);
 		return this.each(function(){
-			new Pagination(_this, opts);
+			new Pagination(_this, opt);
 		});
 	};
 	$.fn.pagination.defaults = {
-		current_page: 1,
-		items_per_page: 2,
-		total_page: 10,
-		load_first_page: function(){},
-		complete: function(){}
+		current_page: 1,  // 当前页
+		items_per_page: 3,	// 每页列表数
+		max_items: 20,  // 总列表数
+		before: $.noop, // 初始化前回调
+		after: $.noop, // 初始化后回调
+		interval: 5 // 页码间隔
 	};
 
-	function Pagination(_this, opts){
-		this.opts = opts;
+	function Pagination(_this, opt){
+		this.opt = opt;
 		this.elem = _this;
-		this.init(opts);
+		this.init();
 	}
 
 	Pagination.prototype = {
-		init: function(opts){
-			this.opts.load_first_page();
-			this.eventListner();
+		init: function(){
+			this.opt.before();
+			this.init_handler();
+			this.event_handler();
+		},
+		init_handler: function(){
+			var total = this.get_total_page();
+			if(total < this.opt.interval){
+				this.init_render_html(false, total);
+			}else{
+				this.init_render_html(true, this.opt.interval);
+			}
+		},
+		init_render_html: function(show, total){
+			var elem = this.elem;
+			if(!show){
+				$(elem).append('<span class="prev">上一页</span>');
+				for (var i = 1; i <= total; i++) {
+					$(elem).append('<span data-page="'+i+'">'+i+'</span>');
+				}
+				$(elem).append('<span class="next">下一页</span>');
+			}else{
+				$(elem).append('<span class="prev">上一页</span>');
+				for (var i = 1; i <= total; i++) {
+					if(i == 2){
+						$(elem).append('<span data-page="'+i+'">'+i+'</span>');
+						$(elem).append('<span>...</span>');
+					}else{
+						$(elem).append('<span data-page="'+i+'">'+i+'</span>');
+					}
+				}
+				$(elem).append('<span class="next">下一页</span>');
+			}
+			$(elem).find('span').eq(1).addClass('active');
+			$(elem).find('span').eq(0).addClass('disabled');
+		},
+		get_total_page: function(){
+			var total = Math.ceil( this.opt.max_items / this.opt.items_per_page );
+			return total;
+		},
+		event_handler: function(){
+			var _this = this;
+			var page;
+			var total_page = this.get_total_page();
+
+			this.elem.on('click', 'span.prev', function(){
+				_this.prev();
+			});
+			this.elem.on('click', 'span.next', function(){
+				_this.next();
+			});
+			this.elem.on('click', '[data-page]', function(){
+				page = $(this).data('page');
+				_this.pageto(page);
+			});
+		},
+		pageto: function(page_num){
+			this.update_state(page_num);
+		},
+		update_state: function(page_num){
+			this.elem.find('span').removeClass('active');
+			this.elem.find('[data-page="'+page_num+'"]').addClass('active');
+			this.set_current_page(page_num);
 		},
 		get_current_page: function(){
-			var current_page = this.opts.current_page;
+			var current_page = this.opt.current_page;
 			return parseInt(current_page);
 		},
 		set_current_page: function(current_page){
-			this.opts.current_page = current_page;
-		},
-		get_total_page: function(){
-			return this.opts.total_page;
+			this.opt.current_page = current_page;
 		},
 		prev: function(){
 			var current_page = this.get_current_page();
 			if(current_page == 1){
 				return false;
 			}
-			this.pageto(current_page-1, 'prev');
+			this.pageto(current_page-1);
 		},
 		next: function(){
 			var current_page = this.get_current_page();
@@ -48,92 +106,17 @@
 			if(current_page == total_page){
 				return false;
 			}
-			this.pageto(current_page+1, 'next');
+			if(!$('[data-page="'+(current_page+1)+'"]').length){
+				this.render_html();
+			}
+			this.pageto(current_page+1);
 		},
-		first: function(){
-			this.pageto(1, 'first');
-		},
-		last: function(){
-			var total_page = this.get_total_page();
-			this.pageto(total_page, 'last');
-		},
-		pageto: function(page_num, state){
-			this.update_state(page_num, state);
-			this.set_current_page(page_num);
-			this.opts.complete();
-		},
-		eventListner: function(){
-			var _this = this;
-			var total_page = this.get_total_page();
-			this.elem.on('click', '[data-page]', function(){
-				var page_num = $(this).attr('data-page');
-				if(page_num == 1){
-					_this.first();
-				}else if(page_num == total_page){
-					_this.last();
-				}else if(page_num == 'prev'){
-					_this.prev()
-				}else if(page_num == 'next'){
-					_this.next();
-				}else if(page_num == -1){
-					return false;
-				}else{
-					_this.pageto(page_num);
-				}
+		render_html: function(){
+			this.elem.find('[data-page]').each(function(key, elem){
+				var page = $(elem).data('page');
+				page++;
+				$(elem).attr('data-page', page).html(page);
 			});
-		},
-		update_state: function(page_num, state){
-			var total_page = this.get_total_page();
-			var page = page_num;
-
-			this.render_data(page_num, state);
-
-			this.elem.find('[data-page]').removeClass('active');
-			this.elem.find('[data-page="'+page+'"]').addClass('active');
-
-			if(page_num == 1){
-				this.elem.find('[data-page="prev"]').removeClass('waves-effect').addClass('disabled');
-				this.elem.find('[data-page="next"]').addClass('waves-effect').removeClass('disabled');
-			}else if(page_num == total_page){
-				this.elem.find('[data-page="prev"]').addClass('waves-effect').removeClass('disabled');
-				this.elem.find('[data-page="next"]').removeClass('waves-effect').addClass('disabled');
-			}else{
-				this.elem.find('[data-page="prev"]').addClass('waves-effect').removeClass('disabled');
-				this.elem.find('[data-page="next"]').addClass('waves-effect').removeClass('disabled');
-			}
-		},
-		render_data: function(page_num, state){
-			var total_page = this.get_total_page();
-			var index = this.elem.find('.active').index();
-			if(state == 'first'){
-				this.elem.find('li').eq(3).attr('data-page', page_num+1).find('a').html(page_num+1);
-				this.elem.find('li').eq(4).attr('data-page', page_num+2).find('a').html(page_num+2);
-				this.elem.find('li').eq(5).attr('data-page', page_num+3).find('a').html(page_num+3);
-			}else if(state == 'last'){
-				this.elem.find('li').eq(3).attr('data-page', page_num-3).find('a').html(page_num-3);
-				this.elem.find('li').eq(4).attr('data-page', page_num-2).find('a').html(page_num-2);
-				this.elem.find('li').eq(5).attr('data-page', page_num-1).find('a').html(page_num-1);
-			}else if(state == 'prev'){
-				if(index == 5){
-					this.elem.find('li').eq(3).attr('data-page', page_num-1).find('a').html(page_num-1);
-					this.elem.find('li').eq(4).attr('data-page', page_num).find('a').html(page_num);
-					this.elem.find('li').eq(5).attr('data-page', page_num+1).find('a').html(page_num+1);
-				}else if(page_num < total_page-3 && page_num > 1){
-					this.elem.find('li').eq(3).attr('data-page', page_num).find('a').html(page_num);
-					this.elem.find('li').eq(4).attr('data-page', page_num+1).find('a').html(page_num+1);
-					this.elem.find('li').eq(5).attr('data-page', page_num+2).find('a').html(page_num+2);
-				}
-			}else if(state == 'next'){
-				if(index == 3){
-					this.elem.find('li').eq(3).attr('data-page', page_num-1).find('a').html(page_num-1);
-					this.elem.find('li').eq(4).attr('data-page', page_num).find('a').html(page_num);
-					this.elem.find('li').eq(5).attr('data-page', page_num+1).find('a').html(page_num+1);
-				}else if(page_num > 4 && page_num < total_page){
-					this.elem.find('li').eq(3).attr('data-page', page_num-2).find('a').html(page_num-2);
-					this.elem.find('li').eq(4).attr('data-page', page_num-1).find('a').html(page_num-1);
-					this.elem.find('li').eq(5).attr('data-page', page_num).find('a').html(page_num);
-				}
-			}
 		}
 	}
 })(jQuery);
