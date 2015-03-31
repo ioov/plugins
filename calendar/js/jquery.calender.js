f5f7dbd9792c66ca5416cf8500e8b13932f1a1e0
koala4js <koala4js@gmail.com>
2015-03-31 16:30:57 +0800

    up

--- calendar/js/jquery.calender.js
+++ calendar/js/jquery.calender.js
@@ -25,11 +25,9 @@
 	function Calender(el, opt){
 		this.el = el;
 		this.opt = opt;
-		this.date = {
-			year: opt.year || new Date().getFullYear(),
-			month: opt.month || new Date().getMonth() + 1,
-			date: opt.date || new Date().getDate()
-		}
+		this.year = opt.year || new Date().getFullYear();
+		this.month = opt.month || new Date().getMonth() + 1;
+		this.date = opt.date || new Date().getDate()
 		this.init();
 	};
 
@@ -38,24 +36,38 @@
 			this.renderTmpl();
 			this.eventHandler();
 		},
-		getDatePerMonth: function(date){
-			var _date = new Date(date.year, date.month, 0);
+		getDatePerMonth: function(year, month){
+			var _date = new Date(year, month, 0);
 			return _date.getDate();
 		},
-		getFirstDayPerMonth: function(date){
-			var _date = new Date(date.year, date.month-1, 1);
+		getFirstDayPerMonth: function(year, month){
+			var _date = new Date(year, month-1, 1);
 			return _date.getDay() == 0 ? 7 : _date.getDay();
 		},
+		getCurDate: function(){
+			var _date = {
+				year: new Date().getFullYear(),
+				month: new Date().getMonth()+1,
+				date: new Date().getDate()
+			};
+			return _date;
+		},
+		isCurDate: function(year, month, date){
+			var curDate = this.getCurDate();
+			return curDate.year == year && curDate.month == month && curDate.date == date && true;
+		},
 		renderTmpl: function(){
-			var datePerMonth = this.getDatePerMonth(this.date);
-			var firstDayPerMonth = this.getFirstDayPerMonth(this.date);
+			var datePerMonth = this.getDatePerMonth(this.year, this.month);
+			var firstDayPerMonth = this.getFirstDayPerMonth(this.year, this.month);
+			var prevDatePerMonth = this.getDatePerMonth(this.year, this.month-1);
+			var className = '';
 
 			var tmpl = '<div class="calender-btns">';
 			tmpl += '<a class="calender-prev-year" href="javascript:;">上一年</a>';
-			tmpl += '<span class="calender-year">'+this.date.year+'</span>';
+			tmpl += '<span class="calender-year">'+this.year+'</span>';
 			tmpl += '<a class="calender-next-year" href="javascript:;">下一年</a>';
 			tmpl += '<a class="calender-prev-month" href="javascript:;">上一月</a>';
-			tmpl += '<span class="calender-month">'+this.date.month+'</span>';
+			tmpl += '<span class="calender-month">'+this.month+'</span>';
 			tmpl += '<a class="calender-next-month" href="javascript:;">下一月</a>';
 			tmpl += '</div>';
 
@@ -67,16 +79,34 @@
             tmpl += '</ul>';
             tmpl += '<ol>';
 
-            for (var i = 1; i <= 42; i++) {
-            	if(i < firstDayPerMonth){
-	            	tmpl += '<li></li>';
-            	}else if(i < datePerMonth + firstDayPerMonth){
-	            	tmpl += '<li>'+(i - (firstDayPerMonth - 1) )+'</li>';
-            	}else{
-	            	tmpl += '<li></li>';
-            	}
+
+            for (var i = prevDatePerMonth - (firstDayPerMonth - 1) + 1; i <= prevDatePerMonth; i++) {
+        		if ( this.isCurDate(this.year, this.month-1, i) ){
+	    			className = 'today';
+	    		}else{
+	    			className = '';
+	    		}
+            	tmpl += '<li class="calender-cross '+className+'">'+i+'</li>';
             }
 
+	        for (var i = 1; i <= datePerMonth; i++) {
+	        	if ( this.isCurDate(this.year, this.month, i) ){
+	    			className = 'today';
+	    		}else{
+	    			className = '';
+	    		}
+            	tmpl += '<li class="'+className+'">'+i+'</li>';
+            }
+
+	        for (var i = 1; i <= 42 - ( datePerMonth + ( firstDayPerMonth - 1 ) ); i++) {
+	        	if ( this.isCurDate(this.year, this.month+1, i) ){
+	    			className = 'today';
+	    		}else{
+	    			className = '';
+	    		}
+            	tmpl += '<li class="calender-cross '+className+'">'+i+'</li>';
+            }
+       
 			tmpl += '</ol>';
 			tmpl += '</div>';
 
@@ -85,19 +115,19 @@
 		eventHandler: function(){
 			var _this = this;
 			this.el.on('click', '.calender-next-year', function(){
-				_this.date.year += 1;
+				_this.year += 1;
 				_this.renderTmpl();
 			});
 			this.el.on('click', '.calender-prev-year', function(){
-				_this.date.year -= 1;
+				_this.year -= 1;
 				_this.renderTmpl();
 			});
 			this.el.on('click', '.calender-next-month', function(){
-				_this.date.month += 1;
+				_this.month += 1;
 				_this.renderTmpl();
 			});
 			this.el.on('click', '.calender-prev-month', function(){
-				_this.date.month -= 1;
+				_this.month -= 1;
 				_this.renderTmpl();
 			});
 		}
